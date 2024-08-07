de3e41395b4e6ff2bf41d733ff863baccb1c9e22
Angelina S <soldatenko_angelina@mail.ru>
2024-08-08 00:20:08 +0400

    intial commit

---
+++ js/game.js
@@ -0,0 +1,9 @@
+export function changeHole() {
+    const activeHole = document.querySelector(".active-hole");
+    activeHole.classList.remove("active-hole");
+    const index = Math.floor(1 + Math.random() * 16);
+    const newActive = document.getElementById(`hole${index}`);
+    newActive.classList.add("active-hole")
+}
+
+setInterval(changeHole, 1000)
