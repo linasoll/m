f166891bd320259e4bad2820ff101d6c8612083e
Angelina S <soldatenko_angelina@mail.ru>
2024-08-08 02:07:05 +0400

    new functions

--- js/game.js
+++ js/game.js
@@ -9,36 +9,69 @@ export function changeHole() {
 setInterval(changeHole, 1000)
 
 const startButton = document.querySelector(".start-button");
-
-startButton.addEventListener("click", () => {
-    const popup = document.querySelector(".start-sign");
-    popup.remove();
-    const game = document.querySelector(".game");
-    game.classList.remove("game-hidden")
-})
-
+const game = document.querySelector(".game");
 const points = document.getElementById('points');
 const lost = document.getElementById('lost');
-
 const getHole = index => document.getElementById(`hole${index}`);
+const popup = document.querySelector(".start-sign");
+const popupWin = document.querySelector(".win-sign");
+const popupLose = document.querySelector(".lose-sign");
+const timeValue = document.getElementById("timer");
+const newGameButtons = querySelectorAll(".new-game");
+
+
+startButton.addEventListener("click", () => {
+    popup.remove();
+    game.classList.remove("game-hidden")
+})
 
 for (let i = 1; i < 17; i++) {
     let hole = getHole(i);
     hole.addEventListener("click", function() {
         if (hole.classList.contains("active-hole")) {
             points.textContent++;
-            if (score.textContent === '10') {
-                alert('Победа');
-                score.textContent = '0';
-                lose.textContent = '0';
-            }
         } else {
             lost.textContent++;
             if (lost.textContent === '5') {
-                alert('Поражение');
+                loseGame();
                 score.textContent = '0';
                 lose.textContent = '0';
             }
         } 
     })
+}
+
+
+
+function countDown() {
+    timeValue.textContent--;
+    if (timer.textContent === '0') {
+        winGame();
+        clearInterval(intId);
+        points.textContent = '0';
+        lost.textContent = '0';
+    }
+}
+
+let intId = setInterval(countDown, 1000);
+
+
+function winGame() {
+    game.classList.add("hidden")
+    popupWin.classList.remove("hidden")
+}
+
+function loseGame() {
+    game.classList.add("hidden")
+    popupLose.classList.remove("hidden")
+}
+
+function newGame() {
+    newGameButtons.array.forEach(element => {
+        element.addEventListener("click", () => {
+            game.classList.remove("hidden");
+            popupWin.classList.add("hidden");
+            popupLose.classList.add("hidden")
+        })
+    });
 }
